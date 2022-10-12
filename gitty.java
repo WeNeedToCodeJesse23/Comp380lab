40179472b99931157fc642d5e97913a071e2848e
ICUP69 <jesse.cruz-tinoco.780@my.csun.edu>
2022-10-12 09:31:11 -0700

    Create gitty.java
    java file test added it to repository directory and it popped up allowing me to push.

---
+++ gitty.java
@@ -0,0 +1,11 @@
+package gitest;
+
+public class gitty {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this shit better end up in git");
+
+	}
+
+}
