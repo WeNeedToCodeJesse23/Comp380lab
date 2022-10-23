d590ba5418f11256f23434e0ef2770efcffa5388
WeNeedToCodeJesse23 <113947427+WeNeedToCodeJesse23@users.noreply.github.com>
2022-10-23 00:37:48 -0700

    Login feature Added
    I added a login feature to Rate, Cancel, and Change Windows. It works to an extent,  just sometimes doesn't function when you login. We should probably fix that and just edit the code for that. Some things that might have to get done is making the GUI components of each window and just fixing overall bugs. Feel free to edit/ improve or organize any of the functions or code segments. Sorry if the code is messy btw, for now I was just workin on getting things to work but in the future, we'll improve the code. Maybe

--- Main.java
+++ Main.java
@@ -1,38 +1,47 @@
 package com.company;
 import java.awt.*;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyListener;
-import java.awt.event.KeyEvent;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
 
+
 public class Main extends JFrame {
-//frames
+    //frame setup
     JFrame main;
     JFrame Search;
-    JFrame viewRegister;
     JFrame CancelRoom;
     JFrame ChangeRoom;
     JFrame RateRoom;
     JFrame hotelLogin;
-//buttons
+
+    //button setup
     JButton Make;
-    JButton View;
     JButton Cancel;
     JButton Rate;
     JButton Change;
     JButton Login;
     JButton ReturnMenu;
 
+    //components need for password               
+    JLabel userLb;
+    JTextField userText;
+    JLabel passwordLb;
+    JPasswordField password;
+    boolean locked;
+    int visited; //visited will record the windows visited through integers, to make the conditional statements easier
+    //
+
 
     Main(){
-
     }
 
+    //setup mainframe
     public void createAndShowGUI () {
+        locked = true;
+        //visited = 0;
+
          main = new JFrame("Hotel AOJJ");
          Search = new JFrame("search");
-        // viewRegister = new JFrame("view register");
          CancelRoom = new JFrame("cancel");
          ChangeRoom = new JFrame("change room");
          hotelLogin = new JFrame("Login");
@@ -40,50 +49,47 @@ public class Main extends JFrame {
 
         main.setSize(700,500);
         Search.setSize(700,500);
-        //viewRegister.setSize(700,500);
         CancelRoom.setSize(700,500);
         ChangeRoom.setSize(700,500);
-        hotelLogin.setSize(500,500);
+        hotelLogin.setSize(700,500);
         RateRoom.setSize(700,500);
 
         //main window
         Make = new JButton("Search Reservation");
-        //View = new JButton("View Reservation");
         Cancel = new JButton("Cancel Reservation");
         Rate = new JButton("Rate Reservation");
         Change = new JButton("Change Room");
-        Login = new JButton("Login menu");
+        Login = new JButton("Login");
         ReturnMenu = new JButton("Backspace");
+
 
         Make.setBounds(50,25,150, 40);
        // View.setBounds(200,25,150, 40);
         Cancel.setBounds(350,25,150, 40);
         Rate.setBounds(400,25,150, 40);
         Change.setBounds(500,25,150, 40);
-        Login.setBounds(500,100,150, 40);
-        ReturnMenu.setBounds(200,50,150,40);
+        Login.setBounds(10,80,80, 25);
+        ReturnMenu.setBounds(400,400,100,50);
 
         main.add(Make);
-        //main.add(View);
         main.add(Cancel);
         main.add(Rate);
         main.add(Change);
 
         main.setLayout(new FlowLayout());
-        Search.setLayout(new FlowLayout());
-        //viewRegister.setLayout(new FlowLayout());
-        CancelRoom.setLayout(new FlowLayout());
-        ChangeRoom.setLayout(new FlowLayout());
-        hotelLogin.setLayout(new FlowLayout());
+        Search.setLayout(null);
+        CancelRoom.setLayout(null);
+        ChangeRoom.setLayout(null);
+        RateRoom.setLayout(null);
+        hotelLogin.setLayout(null);
 
         main.setVisible(true);
         Search.setVisible(false);
-       // viewRegister.setVisible(false);
         CancelRoom.setVisible(false);
         ChangeRoom.setVisible(false);
         hotelLogin.setVisible(false);
 
-        // Make.addActionListener(this);
+
         //button Action Listeners
 
         myActionListener e = new myActionListener();
@@ -94,21 +100,38 @@ public class Main extends JFrame {
         Rate.addActionListener(e);
         Change.addActionListener(e);
         ReturnMenu.addActionListener(e);
+       // password.addKeyListener(e);
+
+        Login.addActionListener(e);
+
+
+        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 
     }
 
     //add in this window
     public void RateWindow(){
-        main.setVisible(false);
-        RateRoom.setVisible(true);
-        RateRoom.add(ReturnMenu);
+        visited = 1;
+        if(locked){
+            LoginWindow();
+        } else {
+            main.setVisible(false);
+            RateRoom.setVisible(true);
+            RateRoom.add(ReturnMenu);
+        }
     }
 
     //add in this window
     public void CancelWindow(){
-        main.setVisible(false);
-        CancelRoom.setVisible(true);
-        CancelRoom.add(ReturnMenu);
+        visited = 2;
+        if(locked){
+            LoginWindow();
+        } else {
+            main.setVisible(false);
+            CancelRoom.setVisible(true);
+            CancelRoom.add(ReturnMenu);
+        }
     }
 
     //add in this window
@@ -120,19 +143,50 @@ public class Main extends JFrame {
 
     //add in this window
     public void ChangeWindow(){
-        main.setVisible(false);
-        ChangeRoom.setVisible(true);
-        ChangeRoom.add(ReturnMenu);
+        visited = 3;
+        if(locked){
+            LoginWindow();
+        } else {
+            main.setVisible(false);
+            ChangeRoom.setVisible(true);
+            ChangeRoom.add(ReturnMenu);
+        }
+    }
+
+    //This is the Login window
+    public void LoginWindow(){
+        userLb = new JLabel("User:");
+        userLb.setBounds(10,20,80,25);
+        hotelLogin.add(userLb);
+
+        userText = new JTextField(20);
+        userText.setBounds(100,20,165,25);
+        hotelLogin.add(userText);
+
+        passwordLb = new JLabel("Password:");
+        passwordLb.setBounds(10,50,80,25);
+        hotelLogin.add(passwordLb);
+
+        password = new JPasswordField();
+        password.setBounds(100,50,165,25);
+        hotelLogin.add(password);
+
+        hotelLogin.add(Login);
+        hotelLogin.add(ReturnMenu);
+
+        hotelLogin.setVisible(true);
+        hotelLogin.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {     
         Main test = new Main();
         test.createAndShowGUI();
     }
 
     private class myActionListener implements ActionListener {
 
+        //Button actions
         public void actionPerformed(ActionEvent event) {
             if (event.getSource() == Make) {
                 searchWindow();
@@ -141,12 +195,15 @@ public class Main extends JFrame {
             } else if (event.getSource() == Cancel){
                 CancelWindow();
             } else if (event.getSource() == Rate){
-               RateWindow();
+                RateWindow();
             } else if (event.getSource() == Change) {
                 ChangeWindow();
+            } else if(event.getSource() == Login){
+                LoginPassword(event);
             }
         }
 
+        //Backspace to Home menu function
         public void ReturnToMenuCases (ActionEvent event){
             if ( (RateRoom.isVisible() ) && (event.getSource() == ReturnMenu ) ) {
                 RateRoom.setVisible(false);
@@ -160,9 +217,32 @@ public class Main extends JFrame {
             } else if ((CancelRoom.isVisible() ) && (event.getSource() == ReturnMenu ) ) {
                 CancelRoom.setVisible(false);
                 createAndShowGUI();
+            }else if ((hotelLogin.isVisible() ) && (event.getSource() == ReturnMenu ) ) {
+                hotelLogin.setVisible(false);
             }
+
         }
 
+        //this checks login info and implements the Login action
+        public void LoginPassword (ActionEvent event){
+            String UserTextInput = userText.getText();
+            String passwordInput = password.getText();
+            if(UserTextInput.equals("guest") && passwordInput.equals("pass1")){
+                hotelLogin.setVisible(false);
+                locked = false;
+                switch(visited){
+                    case 1:
+                        RateWindow();
+                        break;
+                    case 2:
+                        CancelWindow();
+                        break;
+                    case 3:
+                        ChangeWindow();
+                        break;
+                }
+            }
+        }
     }
 }
 
