48fbc1237219340e0a5fd9c21b02c50777b7e024
WeNeedToCodeJesse23 <113947427+WeNeedToCodeJesse23@users.noreply.github.com>
2022-10-26 11:02:19 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+
+public class Main  {
+
+
+    public static void main(String[] args) {              //has a connection to sql database
+         GUI test = new GUI();
+        test.createAndShowGUI();
+    }
+
+
+}
