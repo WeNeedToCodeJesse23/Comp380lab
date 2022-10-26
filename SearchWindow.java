f4316aeb83ba889c2e518b5b0aa4584f9c707303
WeNeedToCodeJesse23 <113947427+WeNeedToCodeJesse23@users.noreply.github.com>
2022-10-26 11:02:45 -0700

    Add files via upload

---
+++ SearchWindow.java
@@ -0,0 +1,31 @@
+package com.company;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class SearchWindow extends GUI{
+    JFrame test;
+
+    public void DoSearch() {
+      //  Search.add(ReturnMenu);
+        test = new JFrame("search");
+        test.setSize(700,500);
+       // Search.setLayout(null);
+
+       // main.setVisible(false);
+        test.setVisible(true);
+
+       // ReturnMenu.setBounds(280,400,100,50);
+       // peta.add(ReturnMenu);
+
+    }
+
+
+    SearchWindow(){
+
+    }
+
+
+}
