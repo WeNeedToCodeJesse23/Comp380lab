cae76968f6dfc07a36bedac5780d0f2a34c26fc9
Jaztin <77910637+csun-jtabu@users.noreply.github.com>
2022-10-27 08:57:24 -0700

    group work from 10-26

--- SearchWindow.java
+++ SearchWindow.java
@@ -9,13 +9,12 @@ import java.awt.event.ActionEvent;
 import javax.swing.*;
 import java.awt.event.ActionListener;
 
-public class SearchWindow extends GUI{
+public class SearchWindow extends JFrame {
     JFrame test;
-    JButton backspace;
+    JButton backspace;  
 
-    public void DoSearch() {
-      //  Search.add(ReturnMenu);
-        test = new JFrame("search");
+    public SearchWindow(){
+    	test = new JFrame("search");
         backspace = new JButton("Back Space");
         backspace.setBounds(EXIT_ON_CLOSE, ABORT, WIDTH, HEIGHT);
         test.setSize(700,500);
@@ -25,21 +24,24 @@ public class SearchWindow extends GUI{
         test.setVisible(true);
         test.add(backspace);
         backspace.setVisible(true);
-        
+      
+        myActionListener Dothis = new myActionListener();
+        backspace.addActionListener(Dothis);
+    }
 
-       // ReturnMenu.setBounds(280,400,100,50);
-       // peta.add(ReturnMenu);
+  //read the text file 
+    public ArrayList<HotelRoom> loadData() { //Invalid method declaration; return type required
+      int reg = 0;
+      int lux = 0;
         
-        int reg = 0;
-        int lux = 0;
-        
-        int roomID = 0;
-        boolean roomStatus = false;
-        String roomType;
-        int numberOfRooms = 0;
-        ArrayList<HotelRoom> roomList = new ArrayList<HotelRoom>();
-        Scanner txtInput = null;
-	    try
+      int roomID = 0;
+      boolean roomStatus = false;
+      String roomType;
+      int numberOfRooms = 0;
+      ArrayList<HotelRoom> roomList = new ArrayList<HotelRoom>();
+      Scanner txtInput = null;
+	    
+      try
 	    {
 	       txtInput = new Scanner(new File("RoomReservationTestDataHotelRooms.txt"));
 	       txtInput.useDelimiter(",");
@@ -56,42 +58,71 @@ public class SearchWindow extends GUI{
         	String[] roomArray = roomInfo.split(",");
         	roomID = Integer.parseInt(roomArray[0]);
         	roomStatus = Boolean.parseBoolean(roomArray[1]);
-            roomType = roomArray[2];
-            numberOfRooms = Integer.parseInt(roomArray[3]);
+          roomType = roomArray[2];
+          numberOfRooms = Integer.parseInt(roomArray[3]);
         	HotelRoom room = new HotelRoom(roomID, roomStatus, roomType, numberOfRooms);
-        	if(room.getRoomStatus()) {
-        		if(room.getRoomType().equals("Regular")) {
-        			reg++;
-        		}
-        		else {
-        			lux++;
-        		}
-        	}
         	roomList.add(room);
         	//System.out.println(roomList);
         }
-        txtInput.close();
-        if(reg<80){
-        	
+      return roomList; 
+    }
+
+  //check if there are available rooms left for regular rooms
+    public int numAvailReg(ArrayList<HotelRoom> roomList)
+    {
+      int reg = 0;
+      for(int counter = 0; counter < roomList.size(); counter++)
+      {
+        if( (roomList.get(counter).getRoomStatus() == false) && (roomList.get(counter).getRoomType().equals("Regular")) )
+        {
+            reg++;
+        		/*if(room.getRoomType().equals("Regular")) 
+            {
+        			reg++;
+              return reg;
+            }*/
         }
-        
-        
-
+      }
+      return reg;
     }
 
-
-    public SearchWindow(){
-    	test = new JFrame("search");
-        backspace = new JButton("Back Space");
-        backspace.setBounds(EXIT_ON_CLOSE, ABORT, WIDTH, HEIGHT);
-        test.setSize(700,500);
-       // Search.setLayout(null);
-
-       // main.setVisible(false);
-        test.setVisible(true);
-        test.add(backspace);
-        backspace.setVisible(true);
+  //checks if there are available rooms left for luxury room
+    public int numAvailLux(ArrayList<HotelRoom> roomList)
+    {
+      int lux = 0;
+      for(int counter = 0; counter < roomList.size(); counter++)
+      {
+        if( (roomList.get(counter).getRoomStatus() == false) && (roomList.get(counter).getRoomType().equals("Luxury")) )
+        {
+            lux++;
+        		/*if(room.getRoomType().equals("Regular")) 
+            {
+        			reg++;
+              return reg;
+            }*/
+        }
+      }
+      return lux;
     }
 
+  //make actions 
+   private class myActionListener implements ActionListener {
 
+        //Button actions
+        public void actionPerformed(ActionEvent event) {
+             if (event.getSource() == backspace) {
+                test.setVisible(false);
+                GUI goBack = new GUI();
+                goBack.createAndShowGUI();
+            }
+        }
+
+
+     
+    }
+
+  
 }
+
+
+
