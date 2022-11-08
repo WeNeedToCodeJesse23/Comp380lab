a7808a9780ee478a14a4ba850c11d2c1927401b7
WeNeedToCodeJesse23 <113947427+WeNeedToCodeJesse23@users.noreply.github.com>
2022-11-07 18:57:51 -0800

    Made method CheckPass which is a boolean check as well as returns Current Customer info location.

--- Customer.java
+++ Customer.java
@@ -14,6 +14,7 @@ public class Customer {
 	private int custID;  //login password
 	private int roomID;
 	private static ArrayList<Customer> customerList = new ArrayList<Customer>();
+	private String pass; //idk if we'll be needing this because the login password will be customerID
 	
 	public Customer(String name, String address, String email, String cardNumber)
 	{
@@ -39,6 +40,8 @@ public class Customer {
 				this.roomID = 1 + (int)(Math.random() * ((80 - 1) + 1));
 			}
 		}
+		       
+	        pass = String.valueOf(custID);
 		
 	}
 	
@@ -58,8 +61,15 @@ public class Customer {
 	public void setPass(String pass) {
 		this.pass = pass;
 	}
-	private String pass; //idk if we'll be needing this because the login password will be customerID
 	
+	public static int CheckPass(String Name, String pw){
+        for(int i = 0; i< customerList.size(); i++){
+            if((customerList.get(i).getName().equals(Name)) && String.valueOf(customerList.get(i).getCustID()).equals(pw)) {
+                return i;
+            }
+        }
+        return -1;
+       }
 	
 	public int getRoomID() {
 		return roomID;
