fe7b19cf6ffd7a014e9c191f2c672c15bc31c163
WeNeedToCodeJesse23 <113947427+WeNeedToCodeJesse23@users.noreply.github.com>
2022-11-26 19:57:02 -0800

    Update HotelRoomTest.java

--- HotelRoomTest.java
+++ HotelRoomTest.java
@@ -5,6 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HotelRoomTest {
+	
+    HotelRoom test1 = new HotelRoom(10,false, "Regular",3);
+    HotelRoom test2 = new HotelRoom(20,false, "Regular",3);
+    HotelRoom test3 = new HotelRoom(30,false, "Luxury",5);
+    HotelRoom test4 = new HotelRoom(40,false, "Regular",3);
+    HotelRoom test5 = new HotelRoom(50,true, "Regular",3);
+    HotelRoom test6 = new HotelRoom(60,true, "Luxury",5);
+	
+    ArrayList<HotelRoom> testy = new ArrayList();
 
 	HotelRoom testRoom = new HotelRoom(55, false, "Regular", 2);
 	
@@ -58,6 +67,17 @@ class HotelRoomTest {
 
     @Test
     void updateRoomStatusTest() {
+	testy.add(test1);
+        testy.add(test2);
+        testy.add(test3);
+        testy.add(test4);
+        testy.add(test5);
+        testy.add(test6);
+
+        assertEquals(false,test2.getRoomStatus());
+        HotelRoom.UpdateRoomStatus(test2.getRoomID());
+        assertEquals(true,test2.getRoomStatus());
+
     }
 
     @Test
@@ -66,10 +86,29 @@ class HotelRoomTest {
 
     @Test
     void numAvailRegTest() {
+	testy.add(test1);
+        testy.add(test2);
+        testy.add(test3);
+        testy.add(test4);
+        testy.add(test5);
+        testy.add(test6);
+
+        assertEquals(3,HotelRoom.numAvailReg());
     }
 
     @Test
     void numAvailLuxTest() {
+	testy.add(test1);
+        testy.add(test2);
+        testy.add(test3);
+        testy.add(test4);
+        testy.add(test5);
+        testy.add(test6);
+
+        assertEquals(1,HotelRoom.numAvailLux());
+
+        HotelRoom test7 = new HotelRoom(66,false,"Luxury",5);
+        testy.add(test7);
     }
 
     @Test
